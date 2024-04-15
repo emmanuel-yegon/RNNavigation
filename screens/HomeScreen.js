937e33200c99f4e0741ec5bd39a0c3d8bb8579a1
emmanuel-yegon <kptooman@gmail.com>
2024-04-15 10:58:49 +0300

    designed native stack navigator

---
+++ screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function HomeScreen({ navigation, route }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Text style={styles.text}>{route.params?.result}</Text>
+      <Button
+        title="Go to About"
+        onPress={() =>
+          navigation.navigate("About", {
+            name: "Manu",
+          })
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+});
